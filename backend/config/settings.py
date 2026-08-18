0d12042adc9de4d277c91f9eb417d7f9b2685fda
agnel <agnelglanto07@gmail.com>
2026-08-18 08:18:08 +0530

    backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -108,7 +108,9 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://todo-henna-delta-51.vercel.app'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
+    'https://todo-henna-delta-51.vercel.app'
 ]
