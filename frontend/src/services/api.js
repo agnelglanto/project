ba8e40e852d3b6ed18cef11d1db673294cdb255b
agnel <agnelglanto07@gmail.com>
2026-08-18 08:24:07 +0530

    backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = 'https://project-1-5vzx.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
